3dc8eaa63237c35790b518fcfca6d503bc14f852
Axel Fuentes <69703441+AxelFuentes@users.noreply.github.com>
2020-08-15 21:51:54 -0500

    Create HolaMundo.cs

---
+++ HolaMundo.cs
@@ -0,0 +1,38 @@
+//Eliminar los /* y */ para que sirvan los ejemplos.
+
+//Ejemplo simple
+/*
+class Program
+{
+  static void Main(string[] args)
+  {
+    System.Console.WriteLine("Hola mundo");
+  }
+}
+*/
+//Usando System
+/*
+using System;
+
+class Program 
+{
+  static void Main (string[] args)
+  {
+    Console.WriteLine ("Hello World");
+  }
+}
+*/
+//Usando namespace
+/*
+using System;
+namespace ConsoleAplication1
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Hola mundo");
+    }
+  }
+}
+*/
